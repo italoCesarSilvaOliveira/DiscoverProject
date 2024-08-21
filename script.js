8dc8eca79017b9f111063b122f79e02d42a11317
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2024-08-21 11:15:27 -0300

    add | Added logic for changing background

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    // if (html.classList.contains('light')){
+    //     html.classList.remove('light')
+    // } else {
+    //     html.classList.add('light')
+    // }
+
+    html.classList.toggle('light')
+}
+
+// * Comentários Sobre Lógica de Programação
+// - 1. A classe toggle já realiza toda a lógica que foi
+// feita no primeiro if
